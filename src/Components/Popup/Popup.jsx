4f49161a086d2312104de04b2b3268a5dff281c3
Robin Zhao <titanen.1987@hotmail.com>
2022-10-21 01:05:58 +0200

    hallo test

--- src/Components/Popup/Popup.jsx
+++ src/Components/Popup/Popup.jsx
@@ -14,6 +14,7 @@ class Popup extends React.Component{
     updateProducts = () => {
         if(this.state.input !== ""){
             this.props.addButtonClicked(this.state.input);
+            console.log("hallo!");
         }
   
     }
